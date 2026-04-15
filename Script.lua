b7701c82449136d62dbd4cddd3d921caae5a1ad1
kyrayy <qwx000@mail.ru>
2026-04-15 19:36:42 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,11 @@
+local db_url = "https://github.com/kyrayy/Adopt-Me-Script/blob/main/DataBase.lua"
+local whitelist = loadstring(game:HttpGet(db_url))()
+
+if whitelist[game.Players.LocalPlayer.UserId] then
+    print("Доступ разрешен!")
+    
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+    
+else
+    game.Players.LocalPlayer:Kick("Тебя нет в вайтлисте!")
+end
